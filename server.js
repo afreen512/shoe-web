c6fb3e2e5e55124b502a7d6bdb257a51e5ba020c
nazle786 <64862781+nazle786@users.noreply.github.com>
2020-05-07 12:26:52 -0700

    pagination

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://afreen:afreen@cluster0-6nwcm.mongodb.net/test?retryWrites=true&w=majority';
+'mongodb+srv://afreen:afreen@cluster0-6nwcm.mongodb.net/test?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
